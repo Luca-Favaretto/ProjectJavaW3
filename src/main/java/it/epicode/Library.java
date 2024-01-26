8ca90936369dab75e3cee9b1cf2cf426a091c253
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2024-01-26 18:46:15 +0100

    Add catch and close Entity Manager

--- src/main/java/it/epicode/Library.java
+++ src/main/java/it/epicode/Library.java
@@ -19,77 +19,82 @@ public class Library {
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
+        try {
+            LoanElementDAO loanElementDAO = new LoanElementDAO(em);
+            UserDAO userDAO = new UserDAO(em);
+            LoanDAO loanDAO = new LoanDAO(em);
 
-        LoanElementDAO loanElementDAO = new LoanElementDAO(em);
-        UserDAO userDAO = new UserDAO(em);
-        LoanDAO loanDAO = new LoanDAO(em);
+            System.out.println();
+            System.out.println("Save loanElement and user");
 
-        System.out.println();
-        System.out.println("Save loanElement and user");
+            for (int i = 0; i < 3; i++) {
+                Book book = Faker.newBook.get();
+                loanElementDAO.save(book);
+                Magazine magazine = Faker.newMagazine.get();
+                loanElementDAO.save(magazine);
+                User user = Faker.newUser.get();
+                userDAO.save(user);
+                loanDAO.save(new Loan(user, i % 2 == 0 ? book : magazine));
+            }
 
-        for (int i = 0; i < 3; i++) {
-            Book book = Faker.newBook.get();
-            loanElementDAO.save(book);
-            Magazine magazine = Faker.newMagazine.get();
-            loanElementDAO.save(magazine);
-            User user = Faker.newUser.get();
-            userDAO.save(user);
-            loanDAO.save(new Loan(user, i % 2 == 0 ? book : magazine));
+
+            System.out.println("---------------------------------------------------");
+
+            System.out.println("Find and delete by UUID");
+            System.out.println();
+
+            UUID uuidElem = UUID.fromString("0441225b-ab0c-4106-bc67-acb7574ce966");
+
+
+            System.out.println(loanElementDAO.findById(uuidElem));
+            loanElementDAO.findByIdAndDelete(uuidElem);
+
+            System.out.println("---------------------------------------------------");
+
+            System.out.println("Find by Year of Publication");
+            System.out.println();
+
+            Faker.isEmpty(loanElementDAO.findByYearPublicatin(1990)).forEach(System.out::println);
+            System.out.println();
+
+            System.out.println("---------------------------------------------------");
+
+            System.out.println("Find by Year of Author");
+            System.out.println();
+
+            Faker.isEmpty(loanElementDAO.findByYearAuthor("Omer Thompson DDS")).forEach(System.out::println);
+            System.out.println();
+
+            System.out.println("---------------------------------------------------");
+
+            System.out.println("Find by Year of Title");
+            System.out.println();
+
+            Faker.isEmpty(loanElementDAO.findByYearTitle("the")).forEach(System.out::println);
+            System.out.println();
+
+            System.out.println("---------------------------------------------------");
+
+            System.out.println("Find loan by cardNumber");
+            System.out.println();
+
+            Faker.isEmpty(loanDAO.findLoanByCardNumber("ac15c67e-bc23-4414-b568-2538be786dbd")).forEach(System.out::println);
+
+            System.out.println();
+            System.out.println("---------------------------------------------------");
+
+
+            System.out.println("Find loan by ExpiredLoan");
+            System.out.println();
+
+            Faker.isEmpty(loanDAO.findExpiredLoan()).forEach(System.out::println);
+
+        } catch (Exception ex) {
+            System.err.println("Error type: " + ex);
+        } finally {
+            em.close();
+            emf.close();
         }
-
-
-        System.out.println("---------------------------------------------------");
-
-        System.out.println("Find and delete by UUID");
-        System.out.println();
-
-        UUID uuidElem = UUID.fromString("0441225b-ab0c-4106-bc67-acb7574ce966");
-
-
-        System.out.println(loanElementDAO.findById(uuidElem));
-        loanElementDAO.findByIdAndDelete(uuidElem);
-
-        System.out.println("---------------------------------------------------");
-
-        System.out.println("Find by Year of Publication");
-        System.out.println();
-
-        Faker.isEmpty(loanElementDAO.findByYearPublicatin(1990)).forEach(System.out::println);
-        System.out.println();
-
-        System.out.println("---------------------------------------------------");
-
-        System.out.println("Find by Year of Author");
-        System.out.println();
-
-        Faker.isEmpty(loanElementDAO.findByYearAuthor("Omer Thompson DDS")).forEach(System.out::println);
-        System.out.println();
-
-        System.out.println("---------------------------------------------------");
-
-        System.out.println("Find by Year of Title");
-        System.out.println();
-
-        Faker.isEmpty(loanElementDAO.findByYearTitle("the")).forEach(System.out::println);
-        System.out.println();
-
-        System.out.println("---------------------------------------------------");
-
-        System.out.println("Find loan by cardNumber");
-        System.out.println();
-
-        Faker.isEmpty(loanDAO.findLoanByCardNumber("ac15c67e-bc23-4414-b568-2538be786dbd")).forEach(System.out::println);
-
-        System.out.println();
-        System.out.println("---------------------------------------------------");
-
-
-        System.out.println("Find loan by ExpiredLoan");
-        System.out.println();
-
-        Faker.isEmpty(loanDAO.findExpiredLoan()).forEach(System.out::println);
-
-
     }
 
 }
